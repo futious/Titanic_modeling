5bbafa4f424257f09087244ec62aadc0e6a66fef
Korey Nishimoto <koreynishimoto@koreys-imac.lan>
2021-10-01 21:04:26 -1000

    Decision tree

--- Titanic_modeling.py
+++ Titanic_modeling.py
@@ -10,25 +10,61 @@ Some words
 import pandas as pd
 
 import numpy as np
-import scipy 
-import joblib
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+from numpy import mean
+from numpy import std
 
-from sklearn import datasets
-from sklearn import metrics
+import matplotlib.pyplot as plt
+
 from sklearn.linear_model import LogisticRegression 
-lr = LogisticRegression(max_iter=200,C=1)
+lr = LogisticRegression(max_iter=1000,C=1)
+
 
 from sklearn.naive_bayes import GaussianNB
 g=GaussianNB()
 
 from sklearn.svm import SVC 
-clf = SVC(max_iter=10000, C=1000)
+clf = SVC(max_iter=100000, C=1000)
 
 from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import train_test_split 
 
+
+from numpy import array
+from sklearn.model_selection import KFold
+
+from sklearn.model_selection import cross_val_score
+from sklearn.datasets import make_classification
+
+
+from sklearn import tree
+dt = tree.DecisionTreeClassifier()
+
+
+
+#################################################################################################
+########################################################################
+########################################################################
+
+
+train_df = pd.read_csv('/Users/koreynishimoto/Desktop/Titanic/'
+                   +'train.csv', index_col=False)
+
+y_col = 'Survived'
+
+train_df['Sex'] = train_df['Sex'].map({'male': 1, 'female': 0})
+
+
+train_df[['Embarked_C', 'Embarked_Q', 'Embarked_S', 'Embarked_nan']] = pd.get_dummies(train_df.Embarked.astype(str), prefix='Embarked')
+
+train_df = train_df.drop(columns=['Name', 'PassengerId', 'Ticket', 'Embarked'])
+
+X_cols = [x for x in train_df if x != y_col]
+
+##################################################################################################
+########################################################################
+########################################################################
+
+
+#GRID TESTING
 
 # set the paramaters that you want to test
 param_grid = {'C': [0.1, 1, 10, 100,1000],  
@@ -41,12 +77,9 @@ grid = GridSearchCV(lr, param_grid, refit = True, verbose = 3,n_jobs=-1)
 
 
 
-train_df = pd.read_csv('/Users/koreynishimoto/Desktop/Titanic/'
-                   +'train.csv', index_col=False)
 
-y_col = 'Survived'
 
-train_df['Sex'] = train_df['Sex'].map({'male': 1, 'female': 0})
+
 
 
 
@@ -100,11 +133,6 @@ train_df['Cabin 76-150'] = ( (train_df['Cabin']<=150) & (train_df['Cabin']>=76) 
 
 
 
-train_df[['Embarked_C', 'Embarked_Q', 'Embarked_S', 'Embarked_nan']] = pd.get_dummies(train_df.Embarked.astype(str), prefix='Embarked')
-
-train_df = train_df.drop(columns=['Name', 'PassengerId', 'Ticket', 'Embarked'])
-
-X_cols = [x for x in train_df if x != y_col]
 
 #train_df['Age'] = train_df['Age'].fillna(0)
 train_df['Age'] = train_df['Age'].fillna(train_df['Age'].mean())
@@ -115,25 +143,43 @@ lr.fit(train_df[X_cols], train_df[y_col])
 
 train_df['prediction'] = lr.predict(train_df[X_cols])
 
-print('Accuracy for logistic regression is', (train_df[y_col] == train_df['prediction']).mean())
+print('\n Accuracy for logistic regression is', (train_df[y_col] == train_df['prediction']).mean())
+
+
+
+
+###################Kfold testing
+
+kfold = KFold(3, shuffle=True,random_state=1)
+
+train_df4= train_df.drop(columns=['prediction'])
+
+testX_cols = [x for x in train_df4 if x != y_col]
+
+
+
+
+#K fold cross examination for Logistical regression model
+# evaluate model
+lr_scores = cross_val_score(lr, train_df4[testX_cols], train_df4[y_col], scoring='accuracy', cv=kfold, n_jobs=-1)
+
+lr_test=[]
+lr_test.append(lr_scores)
+# report performance
+
+print('\n Logistic Regression Accuracy: %.3f (%.3f)' % (mean(lr_scores), std(lr_scores)))
+
+print(lr_test)
+
 
 
     
-'''
-#################################
-
-train_df2= train_df.drop(columns=['prediction'])
-
-gX_cols = [x for x in train_df2 if x != y_col]
-
-g.fit(train_df2[gX_cols], train_df2[y_col])
-
-train_df2['Gaus prediction'] = g.predict(train_df2[gX_cols])
-print('Accuracy for Naive Gaussian is', (train_df2[y_col] == train_df2['Gaus prediction']).mean())
 
 
 #random forest
-'''
+
+
+
 
 #SVC testing
 train_df2= train_df.drop(columns=['prediction'])
@@ -143,20 +189,70 @@ gX_cols = [x for x in train_df2 if x != y_col]
 clf.fit(train_df2[gX_cols], train_df2[y_col])
 
 train_df2['SVC prediction'] = clf.predict(train_df2[gX_cols])
-print('Accuracy for SVC is', (train_df2[y_col] == train_df2['SVC prediction']).mean())
+print('\n Accuracy for SVC is', (train_df2[y_col] == train_df2['SVC prediction']).mean())
+
+
+#K fold cross examination for SVC model
+clf_scores = cross_val_score(clf, train_df4[testX_cols], train_df4[y_col], scoring='accuracy', cv=kfold, n_jobs=-1)
+
+clf_test=[]
+clf_test.append(clf_scores)
+# report performance
+
+print('\n Support Vector Machines Accuracy: %.3f (%.3f)' % (mean(clf_scores), std(clf_scores)))
+
+print(clf_test)
 
 
 
 
 
+#################################################################################################
+########################################################################
+########################################################################
 
+
+#Decision tree
+
+train_df5= train_df.drop(columns=['prediction'])
+
+dtX_cols = [x for x in train_df if x != y_col]
+
+dt.fit(train_df5[gX_cols], train_df5[y_col])
+
+
+
+train_df5['Decission Tree prediction'] = dt.predict(train_df5[gX_cols])
+print('\n Accuracy for Decission Tree is', (train_df5[y_col] == train_df5['Decission Tree prediction']).mean())
+
+#Decision tree cross examination for SVC model
+
+testX_cols = [x for x in train_df5 if x != y_col]
+
+dt_scores = cross_val_score(dt, train_df5[testX_cols], train_df5[y_col], scoring='accuracy', cv=kfold, n_jobs=-1)
+
+dt_test=[]
+dt_test.append(dt_scores)
+# report performance
+
+print('\n Decision tree Accuracy: %.3f (%.3f)' % (mean(dt_scores), std(dt_scores)))
+
+print(dt_test)
+
+
+#################################################################################################
+########################################################################
+########################################################################
+#GRID TESTING 
+
+'''
 # paramater changing
 
 train_df3= train_df.drop(columns=['prediction'])
 
 testX_cols = [x for x in train_df3 if x != y_col]
 
-grid.fit(train_df[testX_cols], train_df[y_col])
+grid.fit(train_df3[testX_cols], train_df[y_col])
 
 train_df3['test prediction'] = grid.predict(train_df3[testX_cols])
 
@@ -173,6 +269,8 @@ print(grid.best_estimator_)
 #print best estimators for individuals
 print(grid.best_estimator_.max_iter)
 print(grid.best_estimator_.C)
-
+'''
+    
+    
 
 
